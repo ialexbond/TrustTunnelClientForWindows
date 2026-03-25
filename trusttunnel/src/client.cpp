8a6bde7c7a240ab88d2d52d8dcc596c24bf73661
Andrey Yakushin <a.yakushin@adguard.com>
2026-03-25 13:14:13 +0000

    Pull request 653: Protect socket on linux with iface from network manager
    Squashed commit of the following:
    
    commit f0225c5836082efd3107975633db4390e2c0bf4f
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Wed Mar 25 12:49:18 2026 +0400
    
        Remove debug comment
    
    commit 71db1885bdc7cfefa10842f0e7cbb2f20f88e216
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Wed Mar 25 08:30:18 2026 +0400
    
        Remove unused get_bound_if from TrustTunnelClient
    
    commit 1a82ea3a373d9ff8398fe0a14d42b09218dbf431
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Wed Mar 25 08:30:00 2026 +0400
    
        Manually pass bound_if from config to AutoNetworkMonitor
    
    commit 10bc6ce52eb619031f1045ac4d5e1692678287cc
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Wed Mar 18 16:38:01 2026 +0400
    
        Protect socket on linux with iface from network manager

--- trusttunnel/src/client.cpp
+++ trusttunnel/src/client.cpp
@@ -74,13 +74,6 @@ bool TrustTunnelClient::process_client_packets(VpnPackets packets) {
     return m_vpn && vpn_process_client_packets(m_vpn, packets);
 }
 
-std::string_view TrustTunnelClient::get_bound_if() const {
-    if (const auto *tun = std::get_if<TrustTunnelConfig::TunListener>(&m_config.listener)) {
-        return tun->bound_if;
-    }
-    return {};
-}
-
 Error<TrustTunnelClient::ConnectResultError> TrustTunnelClient::set_system_dns() {
 #ifdef _WIN32
     uint32_t if_index = vpn_win_detect_active_if();
